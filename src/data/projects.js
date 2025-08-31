f3cf0bc1f3ae03edb8f968d149b835897f63c100
JuliyaPershina <jpershina79@gmail.com>
2025-08-31 22:27:50 +0300

    Змінені проекти

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,30 @@
 export default [
   {
-    title: 'ToDo App',
+    title: 'Galleria',
     description:
-      'Зручний таск-менеджер з drag-and-drop, localStorage та темною темою.',
-    demo: 'https://todo-app-demo.vercel.app',
-    code: 'https://github.com/JuliyaPershina/todo-app',
+      'Інтерактивна вебгалерея, яка дозволяє переглядати шедеври світового мистецтва у вигляді сітки мініатюр або у вигляді слайдшоу. Дані про картини підвантажуються з data.json',
+    demo: 'https://juliyapershina.github.io/galleria-slideshow/',
+    code: 'https://github.com/JuliyaPershina/galleria-slideshow',
   },
   {
-    title: 'E-commerce Store',
-    description: 'Інтернет-магазин на React + TypeScript із кошиком і API.',
-    demo: 'https://ecommerce-demo.vercel.app',
-    code: 'https://github.com/JuliyaPershina/ecommerce-store',
+    title: 'Rock, Paper, Scissors, Lizard, Spock – Solution',
+    description:
+      'Це рішення до бонусного варіанту гри Rock, Paper, Scissors, Lizard, Spock, створеної як інтерпретація класичної гри з додатковими виборами.',
+    demo: 'https://juliyapershina.github.io/hw-persina-rock-scissors-paper-lizard-spok/',
+    code: 'hw-persina-rock-scissors-paper-lizard-spok',
   },
   {
-    title: 'Weather App',
+    title: 'IP Address Tracker ',
     description:
-      'Застосунок для перевірки погоди з використанням OpenWeather API.',
-    demo: 'https://weather-app-demo.vercel.app',
-    code: 'https://github.com/JuliyaPershina/weather-app',
+      'interactive web app that allows users to track IP addresses and view their locations on a map.',
+    demo: 'https://ip-address-tracker-alpha-gray.vercel.app/',
+    code: 'https://github.com/JuliyaPershina/IP-address-tracker',
+  },
+  {
+    title: ' Tip Calculator (на TypeScript)',
+    description:
+      'Простий і зручний калькулятор чайових, створений за допомогою TypeScript, HTML і CSS. Дозволяє швидко розрахувати суму чайових і загальну оплату на одну людину.',
+    demo: 'https://juliyapershina.github.io/TS-tip-calculator/',
+    code: 'https://github.com/JuliyaPershina/TS-tip-calculator',
   },
 ];
